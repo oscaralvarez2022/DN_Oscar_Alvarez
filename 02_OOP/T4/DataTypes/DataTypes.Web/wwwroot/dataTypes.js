2ba285a64e4f9b3a8a1771bd0052b36514223975
DN_Oscar_Alvarez <omoises.alvareze@gmail.com>
2022-02-17 15:42:32 -0600

    T4 y E2

---
+++ 02_OOP/T4/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,26 @@
+﻿var element = document.getElementById("example");
+var textExample = "this is a text";
+var aNumber = 10.333;
+
+var complexObject = {
+    name: "Israél",
+    lastName: "Pérez",
+    age: 35
+
+};
+
+//var ul = "<ul>";
+
+//for (var i = 0; i < textExample.length; i++)
+//{
+    //ul += ("<li>" + textExample[i] + "</li>");
+//}
+
+//ul += "</ul>";
+
+
+var data = "133333";
+
+element.innerHTML = typeof(parseInt(data));
+
+
