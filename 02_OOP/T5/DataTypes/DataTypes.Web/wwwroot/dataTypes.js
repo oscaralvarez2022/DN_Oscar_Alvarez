61b77ddffc8eb5ba121b087ca1794d62be421f2b
DN_Oscar_Alvarez <omoises.alvareze@gmail.com>
2022-02-21 10:13:03 -0600

    T5

---
+++ 02_OOP/T5/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,40 @@
+﻿var buttons = document.getElementsByTagName("button");
+//var buttons = document.getElementsByClassName("btn");
+
+function onButtonClick(evt) {
+    var b = evt.currentTarget;
+    var btnName = b.getAttribute("name");
+    var classes = b.classList;
+    var isGreen = false;
+    for (var i = 0; i < classes.length; i++) {
+        var c = classes[i];
+
+        if (c == "greenButton") {
+            isGreen = true;
+        }
+    }
+
+    if (isGreen) {
+        b.className = "btn redButton";
+    } else {
+        b.className = "btn greenButton";
+    }
+
+    var t = document.getElementById("sampleTable");
+    var currentHTML = t.innerHTML;
+    currentHTML += "<tr><td>" + btnName + "</td><td>" + new Date() + "</td></tr>";
+    t.innerHTML = currentHTML;
+
+
+    //alert("Click on: " + buttonName);
+
+    //debugger;
+}
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", onButtonClick);
+}
+
+//debugger;
+
+
---
+++ 02_OOP/T5/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "This is a string";
+            int age = 35;
+            DateTime now = DateTime.Now;
+            double amount = 0;
+
+            string string2 = text + " a second string " + age + now + amount;
+
+            string string3 = string.Format("The age is {0}, the time is {1}, and the amount is {2}", age, now, amount);
+
+            string string4 = $"The age is {age}, the time is {now}, and the amount is {amount}";
+
+            char simpleChar = 'C';
+
+            char[] arrayChar = string4.ToCharArray();
+
+
+            /*for (int i=0; i<arrayChar.Length; i++)
+            {
+                Console.WriteLine(arrayChar[i]);
+            }*/
+
+            /*for (int i = 0; i < string4.Length; i++)
+            {
+                Console.WriteLine(arrayChar[i]);
+            }*/
+
+            /*amount = (double)10 / (double)3;
+            Console.WriteLine(amount.ToString());*/
+
+            /*DateTime dateTime = new DateTime(2000, 1, 1);
+            TimeSpan timeStamp1 = now - dateTime;
+            Console.WriteLine(timeStamp1.TotalMilliseconds);*/
+
+            /*string test = "15";
+            age = int.Parse(test);*/
+
+            string booleanValue = "True";
+            bool isTrue =bool.Parse(booleanValue);
+
+
+
+            // Console.WriteLine(simpleChar);
+
+            Console.ReadKey();
+        }
+    }
+}
