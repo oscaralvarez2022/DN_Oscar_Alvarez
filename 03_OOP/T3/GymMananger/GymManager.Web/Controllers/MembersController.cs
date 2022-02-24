b5e8a4b1d28551f1b39d85a4de5ad1e422f85126
DN_Oscar_Alvarez <omoises.alvareze@gmail.com>
2022-02-24 15:58:25 -0600

    T3
    Interesante el flujo de la información

---
+++ 03_OOP/T3/GymMananger/GymManager.Web/Controllers/MembersController.cs
@@ -0,0 +1,46 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymMananger.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GymMananger.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly iMembersAppService _membersAppService;
+        public MembersController(iMembersAppService membersAppService)
+        {
+            _membersAppService = membersAppService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Member> members = _membersAppService.GetMembers();
+
+            MemberListViewModel viewModel = new MemberListViewModel();
+            viewModel.NewMembersCount = members.Count;
+            viewModel.Members = members;
+
+            return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Member member)
+        {
+            _membersAppService.AddMember(member);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
